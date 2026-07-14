1e57554e705dd65d18078f42a69950bbcd05dba1
Michael Trullas Garcia <adoser2@hotmail.es>
2026-07-14 22:57:09 +0200

    ci

--- ci-docker.sh
+++ ci-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-docker build -f Dockerfile.ci -t deployer-ci .
+docker build -f Dockerfile.ci -t template-ci .
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e TESTCONTAINERS_HOST_OVERRIDE=host.docker.internal \
-    deployer-ci
+    template-ci
