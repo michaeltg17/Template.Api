c26bccbc4aaa17dd1702c418621c883bb944d97c
Michael Trullas Garcia <adoser2@hotmail.es>
2026-08-07 21:22:48 +0200

    tests

--- tests/UnitTests/Application/Features/Products/Models/Requests/Validators/DeleteProductsRequestValidatorTests.cs
+++ tests/UnitTests/Application/Features/Products/Models/Requests/Validators/DeleteProductsRequestValidatorTests.cs
@@ -15,6 +15,7 @@ public sealed class DeleteProductsRequestValidatorTests
         new(nameof(DeleteProductsRequest.Ids), new long[] { 1L }, true) { TestDisplayName = "Ids - Valid: single positive" },
         new(nameof(DeleteProductsRequest.Ids), new long[] { 1L, 2L, 3L }, true) { TestDisplayName = "Ids - Valid: multiple positive" },
         new(nameof(DeleteProductsRequest.Ids), Array.Empty<long>(), false) { TestDisplayName = "Ids - Invalid: empty" },
+        new(nameof(DeleteProductsRequest.Ids), null, false) { TestDisplayName = "Ids - Invalid: null" },
         new(nameof(DeleteProductsRequest.Ids), new long[] { 0L }, false) { TestDisplayName = "Ids - Invalid: zero" },
         new(nameof(DeleteProductsRequest.Ids), new long[] { -1L }, false) { TestDisplayName = "Ids - Invalid: negative" },
         new(nameof(DeleteProductsRequest.Ids), new long[] { 1L, -2L }, false) { TestDisplayName = "Ids - Invalid: mixed positive and negative" },
