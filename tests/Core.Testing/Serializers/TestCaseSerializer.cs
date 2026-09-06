af38488fefd776c952ab2fea54abc956e40ef8e1
Michael Trullas Garcia <adoser2@hotmail.es>
2026-09-06 09:28:04 +0200

    fix: resolve CA1310 warning by using the StringComparison overload of StartsWith

--- tests/Core.Testing/Serializers/TestCaseSerializer.cs
+++ tests/Core.Testing/Serializers/TestCaseSerializer.cs
@@ -154,6 +154,6 @@ namespace Core.Testing.Serializers
             return Activator.CreateInstance(type, vals)!;
         }
 
-        static bool IsValueTuple(Type type) => type.Namespace == "System" && type.Name.StartsWith("ValueTuple`");
+        static bool IsValueTuple(Type type) => type.Namespace == "System" && type.Name.StartsWith("ValueTuple`", StringComparison.Ordinal);
     }
 }
